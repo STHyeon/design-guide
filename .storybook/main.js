ab14eb7bfb863dc88a1e49cdbccb49e7b0abe461
STHyeon <toby2009@naver.com>
2021-03-03 12:33:30 +0900

    [Del] 불필요한 파일/코드 제거

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,4 @@
-const custom = require('../config-overrides');
-
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-a11y'],
-    webpackFinal: (storybookConfig) => {
-        const customConfig = custom(storybookConfig);
-        return {
-            ...storybookConfig,
-            module: { ...storybookConfig.module, rules: customConfig.module.rules }
-        };
-    }
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-a11y']
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // 브라우저 호환
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 
 import React from 'react';
